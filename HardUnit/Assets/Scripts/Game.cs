20824d078abc1f8b2d2f3b820374c85b986c9cb1
DESKTOP-5UOFQ43\paulpettipas <paul.pettipas@gmail.com>
2016-12-10 21:35:30 -0400

    yep thats a pixelaten

--- HardUnit/Assets/Scripts/Game.cs
+++ HardUnit/Assets/Scripts/Game.cs
@@ -5,18 +5,32 @@ using UnityEngine.SceneManagement;
 public class Game : MonoBehaviour {
 
     public static Game Instance;
-
     public int damage = 3;
+    public ScreenWiper wiper;
+
+    bool ended;
 
     public void Awake() {
         damage = 3;
         Instance = this;
     }
 
+    public IEnumerator Start() {
+        yield return StartCoroutine(wiper.WipeIn(32,1));
+        yield break;
+    }
+
     public void Update() {
 
-        if (damage <= 0) {
-            SceneManager.LoadScene("main");
+        if (damage <= 0 && !ended) {
+            ended = true;
+            StartCoroutine(EndGame());
         }
+    }
+
+    public IEnumerator EndGame() {
+        yield return StartCoroutine(wiper.WipeOut(1,32));
+        SceneManager.LoadScene("main");
+        yield break;
     }
 }
---
+++ HardUnit/Assets/Scripts/ScreenWiper.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ScreenWiper : MonoBehaviour {
+
+    public int max = 32;
+    public int amount = 1;
+    RenderTexture reTex;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        reTex = RenderTexture.GetTemporary(src.width / amount, src.width / amount);
+        reTex.filterMode = FilterMode.Point;
+        src.filterMode = FilterMode.Point;
+        Graphics.Blit(src, reTex);
+        Graphics.Blit(reTex, dest);
+        RenderTexture.ReleaseTemporary(reTex);
+    }
+
+
+    public IEnumerator WipeIn(int start, int end) {
+        amount = start;
+        max = end;
+        while (amount > 1) {
+            amount -= 2;
+            if (amount < 1) {
+                amount = 1;
+            }
+            yield return new WaitForSeconds(0.05f);
+        }
+        amount = 1;
+        amount = end;
+        yield break;
+    }
+
+    public IEnumerator WipeOut(int start, int end) {
+        amount = start;
+        max = end;
+        while (amount < max) {
+            amount += 2;
+            yield return new WaitForSeconds(0.05f);
+        }
+        amount = end;
+        yield break;
+    }
+
+    public IEnumerator WipeOut(float time) {
+        float dt = 1 / time;
+        float t = 0;
+        float start = amount;
+        while (t < 1) {
+            t += dt * Time.deltaTime;
+            amount = (int)Mathf.Lerp(start, max, t);
+            Debug.Log(amount);
+            yield return null;
+        }
+        yield break;
+    }
+}
+
