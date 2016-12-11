a4329b363f1441e5801d96cf7c81800ea4dbc80c
DESKTOP-5UOFQ43\paulpettipas <paul.pettipas@gmail.com>
2016-12-10 20:29:26 -0400

    torso swings free

---
+++ HardUnit/Assets/Scripts/MouseCurser.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCurser : MonoBehaviour {
+
+    public Transform curser;
+
+    void Update() {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit) && Hero.Instance != null) {
+            curser.position = new Vector3(hit.point.x, Hero.Instance.transform.position.y, hit.point.z);
+        }
+    }
+}
--- HardUnit/Assets/Scripts/WeaponTorso.cs
+++ HardUnit/Assets/Scripts/WeaponTorso.cs
@@ -6,6 +6,16 @@ public class WeaponTorso : MonoBehaviour {
 
     public List<GameObject> torsos = new List<GameObject>();
 
+    public Transform curser;
+
+    public void Awake() {
+        curser = GameObject.Find("curser").transform;
+    }
+
+    public void Update() {
+        transform.LookAt(curser);
+    }
+
     public void ShowTorso(string wepName) {
         torsos.ForEach(x => {
             x.SetActive(false);
